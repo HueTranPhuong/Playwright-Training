4404def6fea3e8e3cee48f002441180638a93fd4
HueTranPhuong <tranphuonghue2507@gmail.com>
2024-06-04 11:02:54 +0700

    update tc

--- tests/RegisterUser.spec.ts
+++ tests/RegisterUser.spec.ts
@@ -8,19 +8,31 @@ test('Register user', async ({page}) => {
   //
   await page.goto('http://automationexercise.com');
 
+  //Verify that home page is visible successfully
   await expect(page).toHaveTitle(title);
 
+  //Click on 'Signup / Login' button
   await page.locator('//a[contains(text(),"Login")]').click();
 
+  //Verify 'New User Signup!' is visible
   await expect(page.locator('div.signup-form h2')).toBeVisible();
 
+  //Enter name and email address
   await page.locator('input[name="name"]').fill(name);
-
   await page.locator('//form[@action="/signup"]//input[@type="email"]').fill(email);
 
+  //Click 'Signup' button
   await page.locator('button[data-qa="signup-button"]').click();
 
+  //Verify that 'ENTER ACCOUNT INFORMATION' is visible
   await expect(page.locator('//b[text()="Enter Account Information"]')).toBeVisible();
+
+  //Fill details: Title, Name, Email, Password, Date of birth
+  await page.locator('input#id_gender2').click();
+  await page.locator('input#password').fill('123456aA@');
+  await page.locator('input#password').fill('123456aA@');
+
+
 
 });
 
