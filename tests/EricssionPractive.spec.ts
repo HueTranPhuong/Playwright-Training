65d69a3d8d4ce0bfbce6574f85795678b82b095c
HueTr <tranphuonghue2507@gmail.com>
2024-06-20 11:32:57 +0700

    add new tc

--- tests/EricssionPractive.spec.ts
+++ tests/EricssionPractive.spec.ts
@@ -1,24 +1,52 @@
 import { test, expect } from '@playwright/test';
 import { promises } from 'dns';
 
-test('verify pdf file is downloaded successfully', async ({page, headless}) => {
+const downloadFileName = 'modern-slavery-and-human-trafficking-statement.pdf';
 
-    if (headless) {
-
-
-
+test('verify pdf file is downloaded successfully', async ({ page, headless }) => {
     await page.goto("https://www.ericsson.com/en");
 
     await page.locator('a#CybotCookiebotDialogBodyButtonNecessary').click();
 
+    //step 2 scroll den link 
+
     await page.locator('//a[@title="Link to pdf document"]').scrollIntoViewIfNeeded();
 
-    await page.locator('//a[@title="Link to pdf document"]').click();
+    if (headless) {
 
-    //download file pdf
+        // download file c1 dùng doc playwright
+        // const downloadPromise = page.waitForEvent('download');
 
-    promises.all
+        // await page.locator('//a[@title="Link to pdf document"]').click();
+
+        // const download = await downloadPromise;
+
+        // console.log(download.suggestedFilename());
+
+        // expect(download.suggestedFilename()).toEqual(downloadFileName); }
+
+
+        // download file c2 dùng promissAll
+
+        const [download, click] = await Promise.all([
+            page.waitForEvent('download'),
+            page.locator('//a[@title="Link to pdf document"]').click()
+
+        ]);
+        expect(download.suggestedFilename()).toEqual(downloadFileName);
     }
 
-    
+    else {
+        const [newpage] = await Promise.all([
+            page.waitForEvent('load'),
+            page.locator('//a[@title="Link to pdf document"]').click()
+        ])
+        newpage.on('response', async (response) => {
+            expect(response.headers()['content-type']).toBe('application/pdf');
+
+            
+        })
+
+        await newpage.reload();
+    }
 })
